aecb1170e445adc2581fd8756514a2620a83dc1e
Ruslan Tinkov <russelmr@mail.ru>
2020-11-21 21:07:07 +0300

    Added menu animation and detection of section position

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,32 @@
 $(document).ready(function(){
     $(".menu__link").on("click", function (e) {
-        $(".menu__link").removeClass("menu__link_state_active");
-        $(this).addClass("menu__link_state_active");
-
         e.preventDefault();
         const href = $(this).attr("href");
         $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    });
+
+    $(window).scroll(function() {
+        var windowScroll =   $(window).scrollTop();
+        $(".menu__link").removeClass("menu__link_state_active");
+
+        $(".section").each(function(){
+            var elementTop = $(this).offset().top;
+            var elementHeight = $(this).height();
+            if(windowScroll >= elementTop && windowScroll < (elementTop + elementHeight)) {
+                var itemId = $(this).attr("id");
+                $("a[href='#" + itemId + "']").addClass("menu__link_state_active");
+            }
+        });
+       
     });
 });
